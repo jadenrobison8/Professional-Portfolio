d7511787cb6c93fb455c3dc4adf77341d5fe2ebe
robisonja <jadenrobison8@gmail.com>
2021-03-14 20:24:44 -0600

    add project 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
 var contact = document.querySelector("#Contact-me");
 var resume = document.querySelector("#Resume");
 
+//on click download resume
 
+// ideas for portfolio
+    //hide contact information, when contact me is clicked, show info and picture, hide everything else
 
+    //loop through different pieces of work
+        // when work section is clicked, hide everything else, show all work
 
+    //add time and calender?
 
+    //add ability to schedule meeting on calender and save to local storage, and send to me
 
+    //add need for website or hire
 
+    //add a self portrait in there somewhere
 
+    //fundamental needs in a portfolio
+        //who you aree
+        //what you do
+        //your location
+        //the work you have to share
+        //how to contact you
+
+    //divide up into homepage, about me, contact, work etc
+
+    //add a design process page
+
+    //
